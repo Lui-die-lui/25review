b2c676ea4d2c2354e6ccd6643b790c5fb32dddd6
Lui-die-lui <lsg05h@naver.com>
2025-05-14 23:58:58 +0900

    init:first commit

---
+++ src/_01_intro/intro.java
@@ -0,0 +1,10 @@
+package _01_intro;
+
+public class intro {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+        //단축 sout
+//        ctrl+/ = 주석처리 수동 //=주석처리 (선택 범위 내 가능)
+    }
+}
